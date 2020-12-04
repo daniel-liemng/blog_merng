7d4c1cfe822d057ff348d8cf4ed3ee47c49a73c4
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2020-12-04 17:55:27 -0500

    Like Post

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -5,6 +5,7 @@ const checkAuth = require("../../utils/check-auth");
 
 module.exports = {
   Mutation: {
+    // Create Comment
     createComment: async (_, { postId, body }, context) => {
       const { username } = checkAuth(context);
 
@@ -47,6 +48,29 @@ module.exports = {
         } else {
           throw new AuthenticationError("Action Not Allowed");
         }
+      } else {
+        throw new UserInputError("Post Not Found");
+      }
+    },
+    // Like Post
+    likePost: async (_, { postId }, context) => {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          // Post already liked, unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          // Not liked, like post
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
       } else {
         throw new UserInputError("Post Not Found");
       }
