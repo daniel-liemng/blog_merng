cbe966de2dab713e7048a10ca60696dff8078489
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2020-12-04 18:25:45 -0500

    LikeCount, CommentCount

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,6 +3,15 @@ const commentsResolvers = require("./comments");
 const usersResolvers = require("./users");
 
 module.exports = {
+  Post: {
+    likeCount: (parent) => {
+      console.log(parent);
+      return parent.likes.length;
+    },
+    commentCount: (parent) => {
+      return parent.comments.length;
+    },
+  },
   Query: {
     ...postsResolvers.Query,
   },
